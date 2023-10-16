13f5fd1788b77d590f140ea4b55189b72801621c
islivin <slivinigora@gmail.com>
2023-10-16 22:02:35 +0300

    add example work with default exchange

---
+++ RabbitMQ/Consomer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    const string queue = "dev-queue";
+
+    channel.QueueDeclare(
+        queue: queue,
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (sender, e) =>
+    {
+        var body = e.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine($"Recived message - {message}");
+    };
+
+    channel.BasicConsume(
+        queue: queue,
+        autoAck: true,
+        consumer: consumer);
+
+    Console.WriteLine($"Attach consumer to queue - {queue}");
+
+    Console.ReadLine();
+}
---
+++ RabbitMQ/Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    var counter = 0;
+    while (true)
+    {
+        Thread.Sleep(1000);
+        const string queue = "dev-queue";
+
+        channel.QueueDeclare(
+            queue: queue,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var message = $"Hello word! {counter}";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: queue,
+            body: body);
+
+        Console.WriteLine($"Message is sent. {counter}");
+
+        counter++;
+    }
+}
