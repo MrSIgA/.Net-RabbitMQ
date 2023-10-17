21ad69a64606bc01425b2318c102b62d42f58147
islivin <slivinigora@gmail.com>
2023-10-17 21:46:42 +0300

    add example work with fanout exchange

---
+++ RabbitMQ/FanoutExchange/FanoutExchange.Consomer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    const string exchange = "fanout_exchange";
+
+    channel.ExchangeDeclare(
+        exchange: exchange,
+        type: ExchangeType.Fanout);
+
+    var queueName = channel.QueueDeclare().QueueName;
+
+    channel.QueueBind(
+        queue: queueName,
+        exchange: exchange,
+        routingKey: string.Empty);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (sender, e) =>
+    {
+        var body = e.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine($"Recived message - {message}");
+    };
+
+    channel.BasicConsume(
+        queue: queueName,
+        autoAck: true,
+        consumer: consumer);
+
+    Console.WriteLine($"Attach consumer to queue - {queueName}");
+
+    Console.ReadLine();
+}
---
+++ RabbitMQ/FanoutExchange/FanoutExchange.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    var counter = 0;
+    while (true)
+    {
+        Thread.Sleep(1000);
+
+        const string exchange = "fanout_exchange";
+
+        channel.ExchangeDeclare(
+            exchange: exchange,
+            type: ExchangeType.Fanout);
+
+        var message = $"Hello word! {counter}";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(
+            exchange: exchange,
+            routingKey: string.Empty,
+            body: body);
+
+        Console.WriteLine($"Message is sent. {counter}");
+
+        counter++;
+    }
+}
